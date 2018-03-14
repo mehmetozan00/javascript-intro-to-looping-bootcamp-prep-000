5fcadd29cb318c637f04c0ebf65f3805f4c6d037
Ozan Döğüşcü <doguscu_12@hotmail.com>
2018-03-14 15:22:03 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,9 +18,13 @@ function whileLoop(n) {
   return "done";
 }
 
+function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+
   do {
     array.pop();
-  }
-  while (array.length > -1 && maybeTrue());
+  } while (array.length > -1 && maybeTrue());
     return array;
 }
