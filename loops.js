643fd85f5b646190c81e2111b62a373d8e9c27dc
Ozan Döğüşcü <doguscu_12@hotmail.com>
2018-03-14 15:04:05 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,11 +1,10 @@
-forLoop(array){
-  for (let i = 0; i < 25; i++) {
-    if (i===0) {
-      array.push('');
-    }
-    else {
-      array.push('s');
-    }
-    return array;
+function forLoop(array) {
+   for ( let i = 0; i < 25; i++) {
+     if (i === 1) {
+       array.push(`I am 1 strange loop.`)
+     } else {
+       array.push(`I am ${i} strange loops.`)
+     }
   }
+  return array;
 }
